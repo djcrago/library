8e73bd605f5f07b0da7a14ac549f06b15e665372
Dexter Crago <jodieanddexter@gmail.com>
2023-12-11 16:51:49 -0700

    Add example books and a button for a dialog box
    Basic styling was added to li for the sake of testing everything.

---
+++ javascript.js
@@ -0,0 +1,36 @@
+const myLibrary = [
+    {
+        title: 'The Meaning of Marriage',
+        author: 'Timothy Keller',
+        pages: 352,
+        read: true,
+    },
+    {
+        title: 'Range',
+        author: 'David Epstein',
+        pages: 368,
+        read: true,
+    },
+    {
+        title: 'Wild at Heart',
+        author: 'John Eldredge',
+        pages: 222,
+        read: false,
+    },
+];
+
+let list = document.querySelector('#list');
+
+myLibrary.forEach((object) => {
+    let book = document.createElement('li');
+    book.textContent = `${object.title} by ${object.author} is 
+                        ${object.pages} long. Read Status: ${object.read}.`;
+    list.appendChild(book);
+});
+
+let newBook = document.querySelector('#new');
+let dialogBox = document.querySelector('#dialog');
+
+newBook.addEventListener('click', () => {
+    dialogBox.showModal();
+});
