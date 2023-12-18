26aea366cdc4ec4d3343c21275f8a4b831384cf0
Dexter Crago <jodieanddexter@gmail.com>
2023-12-18 10:49:32 -0700

    Reference form inputs and access submitted values
    By using an if statment we are able to check which radio button is
    checked, and then knowing which one is checked we can get that one's
    value.

--- javascript.js
+++ javascript.js
@@ -5,10 +5,28 @@ function Book(title, author, description, pages, read) {
     this.author = author;
     this.description = description;
     this.pages = pages;
-    this.read = Boolean(read);
+    this.read = read;
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+
+
+const form = document.querySelector('form');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const description = document.querySelector('#description');
+const pages = document.querySelector('#pages');
+const read = document.querySelectorAll('.read');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(title.value, author.value, description.value, pages.value);
+    read.forEach((status) => {
+        if(status.checked) {
+            console.log(status.value);
+        };
+    });
+});
